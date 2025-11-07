8dc838b11f274fc237af2f310e4146bc79d4fa36
S7GM <a22310226@ceti.mx>
2025-11-06 19:44:54 -0600

    02_Inserción
    Add insertion sort implementation with comments.

---
+++ Inserción_InsertionSort.py
@@ -0,0 +1,11 @@
+# Tema: Iserción_InsertionSort
+# Nombre: Sofia Galilea Morales Bejarano
+
+# Objetivo de este código:
+# En el presente código se mostrará el método de INSERCIÓN, el cual sirve para que los elementos que van a ir ordenados 
+# son considerados uno a la vez.
+
+for i in range (len(nums)):
+  min_idx=i
+  for j in range  (i+1,len (nums)):
+  if nums [j] < nums[min_idx], nums[i]
